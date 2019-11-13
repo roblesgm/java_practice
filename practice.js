c1bd69722cbe9786bf50c15cc4c2a4929b838570
Grayson Robles <grayson04runner@gmail.com>
2019-11-12 19:26:49 -0700

    Finished java functions

--- practice.js
+++ practice.js
@@ -1,39 +1,80 @@
-cs.innerHTML = "Changed";
+// cs.innerHTML = "Changed";
 
-cs.innerHTML = "<p>Now a Paragraph</p>";
+// cs.innerHTML = "<p>Now a Paragraph</p>";
 
-cs.append("New");
+// cs.append("New");
 
-// setTimeout(() => {
-//     cs.append("Later")
-// }, 2000);
+// // setTimeout(() => {
+// //     cs.append("Later")
+// // }, 2000);
 
-// let x = 0;
+// // let x = 0;
 
 
-// setInterval(() => {
-//     empty.innerHTML = x + " seconds";
-//     x++;
-//     if (x > 5) {
-//         cs.innerHTML = "Done Counting";
-//     } else {
-//         cs.innerHTML = "Not Done Counting";
-//     }
-// }, 1000);
+// // setInterval(() => {
+// //     empty.innerHTML = x + " seconds";
+// //     x++;
+// //     if (x > 5) {
+// //         cs.innerHTML = "Done Counting";
+// //     } else {
+// //         cs.innerHTML = "Not Done Counting";
+// //     }
+// // }, 1000);
 
-let say = function(word) {
-    cs.append("Saying " + word)
-};
+// let say = function(word) {
+//     cs.append("Saying " + word)
+// };
 
-say("hi");
-say("sup");
-say("yo");
-say("hello");
+// say("hi");
+// say("sup");
+// say("yo");
+// say("hello");
 
-let list = [1, 2, 3];
+// let list = [1, 2, 3];
 
-let i = 0
-while(i < list.length); {
-    console.log("Logging" + list[i])
-    i++;
+// let i = 0
+// while(i < list.length); {
+//     console.log("Logging" + list[i])
+//     i++;
+// }
+
+function ready() {
+    console.log("page ready");
+
+    let favs = document.forms.favs;
+
+    let colorEl = favs.elements.color;
+
+    console.log(colorEl.value)
+
+    colorEl.onchange = function() {
+        console.log(colorEl.value);
+        let el = document.createelement("p")
+        el.innerHTML = "You typed " + colorEl.value;
+
+        today.append(el);
+    };
+
+    go.onclick = function() {
+        alert("Please don't go!");
+    }
+
+    weekend.onclick = function() {
+        alert("It's the Weekend!")
+    }
 }
+
+document.addEventListener("DOMContentLoaded", ready);
+
+// console.log("page ready");
+
+//     today.innerHTML = "Tomorrow";
+
+//     let elem = document.createElement('div');
+//     elem.innerHTML = "<p>Hi <strong>There</strong><p>";
+//     elem.className = "special important";
+    
+//     today.prepend(elem);
+
+//     today.classList.add("highlighted");
+//     today.classList.remove("normal");
